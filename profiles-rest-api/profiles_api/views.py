2e55551d4aaff86495c21044815a10f156d4a965
Everton Spader <everton.spader@gmail.com>
2020-08-01 12:11:28 +0200

    Added PUT, PATCH and DELETE methods

--- profiles-rest-api/profiles_api/views.py
+++ profiles-rest-api/profiles_api/views.py
@@ -32,3 +32,17 @@ class HelloAppView(APIView):
                 serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST
             )
+
+    def put(self, request, pk=None):
+        """Handle updateing an object"""
+        # This replaces the object with a new provided object
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        # This uploads only specific fields of an object.
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method': 'DELETE'})
